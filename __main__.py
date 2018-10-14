1ccceeda54c31214ae8bc15049eb68f8dcb4e20a
MC-6312 <mc6312@gmail.com>
2018-10-14 16:44:31 +0300

    1.6.2

--- __main__.py
+++ __main__.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+""" __main__.py
+
+    This file is part of WishCalc.
+
+    WishCalc is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    WishCalc is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with WishCalc.  If not, see <http://www.gnu.org/licenses/>."""
+
 
 if __name__ == '__main__':
     from wishcalc import main
